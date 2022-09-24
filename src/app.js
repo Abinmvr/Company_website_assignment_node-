d86e008480d01d787772b97c012d0b960fb222ff
Abinmvr <abinvmvrcalicut@gmail.com>
2022-09-24 23:57:19 +0530

    login/signup problem fixed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // const cors =require('cors');
 
-app.use(function(req,res,next){
+app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers",'Origin,X-Required-With,Content-Type');
     res.header("Access-Control-Allow-Methods",'*');
